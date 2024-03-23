7307d10750f509e82f8e0f3e40834ec1f68601ee
thresh <17614509521@163.com>
2024-03-23 22:03:05 +0800

    feat: 首页

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,5 @@
 import { View, Text } from "@tarojs/components";
-import { useLoad } from "@tarojs/taro";
-import "./index.scss";
-import { AtButton } from "taro-ui";
 
 export default function Index() {
-  useLoad(() => {
-    console.log("Page loaded.");
-  });
-
-  return (
-    <View className="index">
-      <AtButton type="primary">1235</AtButton>
-    </View>
-  );
+  return <View className="index">首页</View>;
 }
