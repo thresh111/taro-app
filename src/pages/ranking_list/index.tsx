3a88c6f24264ee91f1162e8ed0b7f1c7d17d7c72
thresh <17614509521@163.com>
2024-03-23 22:01:21 +0800

    feat: 天梯榜

---
+++ src/pages/ranking_list/index.tsx
@@ -0,0 +1,10 @@
+import { WebView } from "@tarojs/components";
+
+export default function App() {
+  return (
+    <WebView
+      src="https://topic.expreview.com/GPU/"
+      progressbarColor="#6190E8"
+    />
+  );
+}
